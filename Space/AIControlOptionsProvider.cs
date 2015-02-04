5452e7247bddacd3824a758155ce61df2fe28962
Arne Kiesewetter <banane1996@gmx.net>
2015-02-04 23:16:07 +0100

    Make some adjustments to asteroid count and ship start. Add derpy AIControlOptionsProvider.

---
+++ Space/AIControlOptionsProvider.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space
+{
+    internal class AIControlOptionsProvider : ControlOptionsProvider<ShipControlOptions>
+    {
+        private Random random = new Random();
+
+        public override ShipControlOptions GetControlOption()
+        {
+            var rand = random.Next(0, 10);
+            return (ShipControlOptions)(rand > 2 ? 0 : rand);
+        }
+    }
+}
