6641e2d64d7b33bf40a35c52b46b49a153be875b
Arne Kiesewetter <banane1996@gmx.net>
2015-02-08 17:22:52 +0100

    Add points and made shooting cost points. Separated some stuff out into a static class.

---
+++ Space/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space
+{
+    public static class Game
+    {
+        public static int FieldHeight
+        {
+            get { return Console.WindowHeight - 3; }
+            set { Console.WindowHeight = value + 3; }
+        }
+
+        public static int FieldWidth
+        {
+            get { return Console.WindowWidth; }
+            set
+            {
+                Console.WindowWidth = value;
+                SeparationLine = "".PadLeft(value, '-');
+            }
+        }
+
+        public static string SeparationLine { get; private set; }
+
+        public static int Points { get; set; }
+
+        public static int Shots { get; set; }
+
+        public static int Asteroids { get { return (Game.FieldWidth * Game.FieldHeight) / 60; } }
+    }
+}
